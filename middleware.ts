2c0550737df639b351c16f02360f9819284cf10c
Arif <arifajif@gmail.com>
2026-02-19 03:52:21 +0700

    cron middleware

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,14 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - api/cron (cron jobs) <-- PENTING: Agar cron job tidak kena redirect login
+     * - images extensions (svg, png, jpg, etc)
+     */
+    '/((?!_next/static|_next/image|favicon.ico|api/cron|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
